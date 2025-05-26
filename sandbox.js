d4eed77693e1b447381dc0df4b05cb03a8e03880
rodneybaloyi <rodneybaloyi31@gmail.com>
2025-05-26 21:31:51 +0200

    Adding my files

---
+++ sandbox.js
@@ -0,0 +1,19 @@
+// strings
+//console.log('hello, world');
+//let email = 'rodneybaloyi@gmail.com';
+//let results = email.lastIndexOf('d');
+
+//console.log(results);
+
+//let result = email.replace('b', 'k')
+
+
+let likes = 10
+//let results = likes + 5
+
+//likes++
+//likes -=2
+//likes *=2
+console.log(likes);
+
+// Nan
